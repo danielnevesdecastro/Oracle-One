a15cd0317af8a9b0136f7a67759e6ab2116540e5
Daniel Neves <danielnevesdecastro@gmail.com>
2023-06-09 15:45:44 -0300

    feat: Referencia

--- Especializacao/JAVA/POO/bytebank/src/bytebank/CriaConta.java
+++ Especializacao/JAVA/POO/bytebank/src/bytebank/CriaConta.java
@@ -3,10 +3,23 @@ package bytebank;
 public class CriaConta {
 	public static void main(String[] args) {
 		Conta c1 = new Conta();
-		c1.saldo = 200;
 		c1.titular = "Daniel";
-		
+		c1.saldo = 200;
 		System.out.println(c1.saldo);
+
+		c1.saldo +=100;
+		
+		System.out.println("Primeira Conta tem : " + c1.saldo);
+		
+		Conta c2 = c1;
+		c2.saldo += 50;
+		
+		System.out.println("Segunda conta tem:"+c2.saldo);
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		
+		
 		
 	}
 }
