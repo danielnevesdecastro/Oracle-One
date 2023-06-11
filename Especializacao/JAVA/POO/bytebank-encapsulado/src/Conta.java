852876d9259d1de715217d801752efe7dc2f8350
Daniel Neves <danielnevesdecastro@gmail.com>
2023-06-11 17:39:17 -0300

    GeteSet referencia

--- Especializacao/JAVA/POO/bytebank-encapsulado/src/Conta.java
+++ Especializacao/JAVA/POO/bytebank-encapsulado/src/Conta.java
@@ -31,20 +31,29 @@ public class Conta {
 	public double getSaldo() {
 		return this.saldo;
 	}
-	
+
 	public int getNumero() {
 		return this.numero;
 	}
+
 	public void setNumero(int numero) {
 		this.numero = numero;
 	}
-	
+
 	public int getAgencia() {
 		return this.agencia;
 	}
+
 	public void setAgencia(int agencia) {
 		this.agencia = agencia;
 	}
-	
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+
+	public Cliente getTitular() {
+		return titular;
+	}
 
 }
--- Especializacao/JAVA/POO/bytebank-encapsulado/src/TesteGetESet.java
+++ Especializacao/JAVA/POO/bytebank-encapsulado/src/TesteGetESet.java
@@ -1,10 +1,21 @@
 
 public class TesteGetESet {
-	
+
 	public static void main(String[] args) {
 		Conta conta = new Conta();
+		// conta.numero = 1337;
 		conta.setNumero(1337);
-		
 		System.out.println(conta.getNumero());
+	
+		Cliente daniel = new Cliente();
+		daniel.setNome("Daniel neves");
+		conta.setTitular(daniel);
+		
+		System.out.println(conta.getTitular().getNome());
+		
+		conta.getTitular().setProfissao("Desenvolvedor");
+		
+		System.out.println(conta.getTitular().getProfissao());
+		
 	}
 }
