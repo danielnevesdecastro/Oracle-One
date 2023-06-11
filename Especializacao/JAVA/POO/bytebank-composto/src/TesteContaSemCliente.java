bc3620262e1ed20a7bde56e9a0a79d71118eb183
Daniel Neves <danielnevesdecastro@gmail.com>
2023-06-11 16:59:51 -0300

    Getters e Setters

--- Especializacao/JAVA/POO/bytebank-composto/src/TesteContaSemCliente.java
+++ Especializacao/JAVA/POO/bytebank-composto/src/TesteContaSemCliente.java
@@ -3,7 +3,7 @@ public class TesteContaSemCliente {
 	
 	public static void main(String[] args) {
 		Conta contaDamarcela = new Conta();
-		System.out.println(contaDamarcela.saldo);
+		System.out.println(contaDamarcela.getSaldo());
 		
 		contaDamarcela.titular = new Cliente();
 		contaDamarcela.titular.nome = "Marcela";
--- Especializacao/JAVA/POO/bytebank-composto/src/TesteSacaNegativo.java
+++ Especializacao/JAVA/POO/bytebank-composto/src/TesteSacaNegativo.java
@@ -1,4 +1,18 @@
 
 public class TesteSacaNegativo {
-
+	
+	public static void main(String[] args) {
+		Conta conta = new Conta();
+		conta.deposita(100);;
+		System.out.println("Saldo: "+conta.getSaldo());
+		
+		System.out.println("Sacou ?" +conta.saca(200));
+		System.out.println("Saldo: "+ conta.getSaldo());
+		
+		
+		//proibido
+		//conta.setSaldo(-101);
+		
+		System.out.println("Saldo: "+ conta.getSaldo());
+	}
 }
