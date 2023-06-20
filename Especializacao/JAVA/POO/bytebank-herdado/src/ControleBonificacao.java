e3c412a8b2ba361d3bcf00840dcc3017695615a0
Daniel Neves <danielnevesdecastro@gmail.com>
2023-06-19 23:17:59 -0300

    polimorfismo

---
+++ Especializacao/JAVA/POO/bytebank-herdado/src/ControleBonificacao.java
@@ -0,0 +1,16 @@
+
+public class ControleBonificacao {
+
+	private double soma;
+	
+	
+	public void registra(Funcionario f) {
+		this.soma += f.getBonificacao();
+	}
+	
+	public double getSoma() {
+		return this.soma; 
+	}
+	
+	
+}
---
+++ Especializacao/JAVA/POO/bytebank-herdado/src/TesteReferencias.java
@@ -0,0 +1,26 @@
+
+public class TesteReferencias {
+	
+	public static void main(String[] args) {
+		
+		Gerente g1 = new Gerente();
+		g1.setNome("Daniel");
+		g1.setSalario(5000.0);
+		
+		EditorVideo e1 = new EditorVideo();
+		e1.setNome("Gabriel");
+		e1.setSalario(2500.0);
+		
+		Designer d1 = new Designer();
+		d1.setNome("Mauricio");
+		d1.setSalario(2000.0);
+		
+		ControleBonificacao crtl = new ControleBonificacao();
+		crtl.registra(g1);
+		crtl.registra(e1);
+		crtl.registra(d1);
+		
+		System.out.println(crtl.getSoma());
+		
+	}
+}
