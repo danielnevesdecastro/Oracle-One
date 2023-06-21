2796a6050236586d06e8421f899c2a0113888d3f
Daniel Neves <danielnevesdecastro@gmail.com>
2023-06-20 23:48:04 -0300

    Métodos Abstratos

--- Especializacao/JAVA/POO/bytebank-herdado/src/EditorVideo.java
+++ Especializacao/JAVA/POO/bytebank-herdado/src/EditorVideo.java
@@ -4,6 +4,6 @@ public class EditorVideo extends Funcionario {
 	@Override
 	public double getBonificacao() {
 		// TODO Auto-generated method stub
-		return super.getBonificacao() + 150;
+		return  150;
 	}
 }
