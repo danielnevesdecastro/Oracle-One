3af47687fd9c3b389d637cdaed861572de64c413
Daniel Neves <danielnevesdecastro@gmail.com>
2023-06-26 15:13:59 -0300

    Stack

--- Especializacao/JAVA/POO/bytebank-herdado-conta/src/ContaCorrente.java
+++ Especializacao/JAVA/POO/bytebank-herdado-conta/src/ContaCorrente.java
@@ -1,5 +1,5 @@
 
-public class ContaCorrente extends Conta {
+public class ContaCorrente extends Conta implements Tributavel {
 
 	public ContaCorrente(int agencia, int numero) {
 		super(agencia, numero);
@@ -16,5 +16,11 @@ public class ContaCorrente extends Conta {
 	public void deposita(double valor) {
 		// TODO Auto-generated method stub
 		super.saldo += valor;
+	}
+
+	@Override
+	public double getValorImposto() {
+		// TODO Auto-generated method stub
+		return super.saldo *0.01 ;
 	}
 }
---
+++ Especializacao/JAVA/POO/bytebank-herdado-conta/src/TesteTributaveis.java
@@ -0,0 +1,19 @@
+
+public class TesteTributaveis {
+	
+	public static void main(String[] args) {
+		
+		ContaCorrente cc = new ContaCorrente(222, 323);
+		cc.deposita(100.0);
+		
+		SeguroDeVida seguro = new SeguroDeVida();
+		
+		CalculadorDeImposto calcImp = new CalculadorDeImposto();
+		calcImp.registra(cc);
+		calcImp.registra(seguro);
+		
+		System.out.println(calcImp.getTotalImposto());
+		
+		
+	}
+}
