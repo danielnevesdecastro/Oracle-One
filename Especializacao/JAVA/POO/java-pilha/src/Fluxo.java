ae17c35051c78c8e4024703a6d4865df0f0d32c5
Daniel Neves <danielnevesdecastro@gmail.com>
2023-06-30 22:32:39 -0300

    catch Exception

--- Especializacao/JAVA/POO/java-pilha/src/Fluxo.java
+++ Especializacao/JAVA/POO/java-pilha/src/Fluxo.java
@@ -5,7 +5,7 @@ public class Fluxo {
 		System.out.println("Ini do main");
 		try {
 			metodo1();
-		} catch (ArithmeticException | NullPointerException ex) {
+		} catch (Exception ex) {
 			String msg = ex.getMessage();
 			System.out.println("Exception " + msg);
 			ex.printStackTrace();
@@ -13,19 +13,17 @@ public class Fluxo {
 		System.out.println("Fim do main");
 	}
 
-	private static void metodo1() {
+	private static void metodo1() throws MinhaException {
 		System.out.println("Ini do metodo1");
 		metodo2();
 		System.out.println("Fim do metodo1");
 	}
 
-	private static void metodo2() {
+	private static void metodo2() throws MinhaException {
 		System.out.println("Ini do metodo2");
-	
-		//ArithmeticException ex =new ArithmeticException("Deu Errado");
-		//throw ex;
-		throw new ArithmeticException("Deu XABU");
-		
-		//System.out.println("Fim do metodo2");
+
+		throw new MinhaException("Deu XABU!!!");
+
+		// System.out.println("Fim do metodo2");
 	}
 }
